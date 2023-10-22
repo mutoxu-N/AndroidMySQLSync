cf6bb1367c3531f1f36e03fa3733db4ef9ad8a8c
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2023-10-22 10:15:18 +0900

    ライブラリアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,17 +43,20 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2") // lifecycle for viewmodel
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2") // lifecycle for livedata
+    // lifecycle
+    val lifecycleVersion = "2.6.2"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion") // lifecycle for viewmodel
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion") // lifecycle for livedata
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
 
     // Room
-    val roomVersion = "2.5.2"
+    val roomVersion = "2.6.0"
     ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-runtime:$roomVersion")
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
